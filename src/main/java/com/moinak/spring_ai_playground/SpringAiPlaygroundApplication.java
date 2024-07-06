0594ef7af5c26b7cc4d176ab9d86eea3941bd42f
m01nak <m01nak77@gmail.com>
2024-07-06 23:41:37 +0530

    init

---
+++ src/main/java/com/moinak/spring_ai_playground/SpringAiPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.moinak.spring_ai_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moinak/spring_ai_playground/SpringAiPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moinak.spring_ai_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
